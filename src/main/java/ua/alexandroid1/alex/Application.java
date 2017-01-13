2581fad592c4c08df61d2e1890af045e897c2dc2
Oleksandr <avpavlov96@gmail.com>
2017-01-13 14:39:48 +0300

    logic added

---
+++ src/main/java/ua/alexandroid1/alex/Application.java
@@ -0,0 +1,12 @@
+package ua.alexandroid1.alex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
